f48536586471f6303e991e82dbbf8adb8b86c5e6
mustofa adny <adniadni04@gmail.com>
2025-11-05 11:19:47 +0700

    feat: create workspace inngest function to stored in prisma

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -54,5 +54,29 @@ const syncUserUpdation = inngest.createFunction(
   }
 );
 
+const syncWorkspaceCreation = inngest.createFunction(
+  { id: "sync-workspace-from-clerk" },
+  { event: "clerk/organization.created" },
+  async ({ event }) => {
+    const { data } = event;
+    await prisma.workspace.create({
+      data: {
+        id: data.id,
+        name: data.name,
+        slug: data.slug,
+        ownerId: data.created_by,
+        image_url: data.image_url,
+      },
+    });
+    await prisma.workspaceMember.create({
+      data: {
+        userId: data.created_by,
+        workspaceId: data.id,
+        role: "ADMIN",
+      },
+    });
+  }
+);
+
 // Create an empty array where we'll export future Inngest functions
 export const functions = [syncUserCreation, syncUserDeletion, syncUserUpdation];
