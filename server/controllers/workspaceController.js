5ebb65db262716fd98605329f39b72144feac65a
mustofa adny <adniadni04@gmail.com>
2025-11-05 12:10:04 +0700

    feat: add member of the workspace controller

--- server/controllers/workspaceController.js
+++ server/controllers/workspaceController.js
@@ -34,3 +34,65 @@ export const getUserWorkspaces = async (req, res) => {
     res.status(500).json({ message: error.code || error.message });
   }
 };
+
+export const addMember = async (req, res) => {
+  try {
+    const { userId } = await req.auth();
+    const { email, role, workspaceId, message } = req.body;
+
+    const user = await prisma.user.findUnique({
+      where: { email: email },
+    });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    if (!workspaceId || !role) {
+      return res
+        .status(400)
+        .json({ message: "Workspace ID and role are required" });
+    }
+    if (!["ADMIN", "MEMBER"].includes(role)) {
+      return res.status(400).json({ message: "Invalid role specified" });
+    }
+
+    const workspace = await prisma.workspace.findUnique({
+      where: { id: workspaceId },
+      include: { members: true },
+    });
+
+    if (!workspace) {
+      return res.status(404).json({ message: "Workspace not found" });
+    }
+    if (
+      !workspace.members.find(
+        (member) => member.userId === userId && member.role === "ADMIN"
+      )
+    ) {
+      return res.status(401).json({ message: "Only admins can add members" });
+    }
+
+    const existingMember = workspace.members.find(
+      (member) => member.userId === user.id
+    );
+    if (existingMember) {
+      return res
+        .status(400)
+        .json({ message: "User is already a member of the workspace" });
+    }
+
+    const member = await prisma.workspaceMember.create({
+      data: {
+        userId: user.id,
+        workspaceId,
+        role,
+        message,
+      },
+    });
+
+    res.status(201).json({ member, message: "Member added successfully" });
+  } catch (error) {
+    console.error("Error adding member:", error);
+    res.status(500).json({ message: error.code || error.message });
+  }
+};
