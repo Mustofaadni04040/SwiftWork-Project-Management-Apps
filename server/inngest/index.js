de0553db39accf2816730e75b823eedf55206115
mustofa adny <adniadni04@gmail.com>
2025-11-05 10:33:51 +0700

    feat: update user inngest function to stored in prisma

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -35,5 +35,25 @@ const syncUserDeletion = inngest.createFunction(
   }
 );
 
+// Inngest function to update user data from the database
+const syncUserCreation = inngest.createFunction(
+  { id: "sync-user-from-clerk" },
+  { event: "clerk/user.created" },
+  async ({ event }) => {
+    const { data } = event;
+    await prisma.user.update({
+      where: {
+        id: data.id,
+      },
+      data: {
+        id: data.id,
+        email: data?.email_addresses[0]?.email_address,
+        name: `${data.first_name} ${data.last_name}`,
+        image: data?.image_url,
+      },
+    });
+  }
+);
+
 // Create an empty array where we'll export future Inngest functions
 export const functions = [];
