9953a633ef74b7c2ab0b6bcb0496fec013a19413
mustofa adny <adniadni04@gmail.com>
2025-11-06 11:52:13 +0700

    feat: create comment task controller

---
+++ server/controllers/commentController.js
@@ -0,0 +1,42 @@
+import prisma from "../configs/prisma.js";
+
+export const addComment = async (req, res) => {
+  try {
+    const { userId } = await req.auth();
+    const { content, taskId } = req.body;
+
+    const task = await prisma.task.findUnique({
+      where: { id: taskId },
+    });
+    const project = await prisma.project.findUnique({
+      where: { id: task.projectId },
+      include: { members: { include: { user: true } } },
+    });
+
+    if (!project) {
+      return res.status(404).json({ message: "Project not found" });
+    }
+
+    const member = project.members.find((member) => member.userId === userId);
+
+    if (!member) {
+      return res.status(403).json({
+        message: "You must be a member of the project to add comments",
+      });
+    }
+
+    const comment = await prisma.comment.create({
+      data: {
+        taskId,
+        content,
+        userId,
+      },
+      include: { user: true },
+    });
+
+    res.status(201).json({ comment, message: "Comment added successfully" });
+  } catch (error) {
+    console.error("Error adding comment:", error);
+    res.status(500).json({ message: error.code || error.message });
+  }
+};
